d154f05e60cc551f24c4eb064948c316b916fcfb
JoeH2025 <joseph.robert.huber@gmail.com>
2025-11-30 15:29:51 -0800

    main vars

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 # The main entry point for the program
 # Takes an image and quality level as input.
+
+image_path = "path"
+quality_x = 100 # The quality of the image, based on how many columns there are
