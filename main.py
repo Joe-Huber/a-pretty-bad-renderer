8a944339df3628c71792b8e98fcdcd868ec8a50c
JoeH2025 <joseph.robert.huber@gmail.com>
2025-12-01 12:20:40 -0600

    import and run

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # The main entry point for the program
 # Takes an image and quality level as input.
+from image_loader import render
 
 image_path = "path"
 quality_x = 100 # The quality of the image, based on how many columns there are
 
 if __name__ == "__main__":
-    print("main")
+    img = render(image_path)
+    print(img)
