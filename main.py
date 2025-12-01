7fa45526c09796cec7ec54b3db555cdc85b0de07
JoeH2025 <joseph.robert.huber@gmail.com>
2025-12-01 16:33:00 -0600

    kev img

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from image_loader import render
 from develop_modified_image import generate_new_image
 import matplotlib.pyplot as plt
 
-image_path = "images/860x1033.png"
-quality_x = 100 # The quality of the image, based on how many columns there are
+image_path = "images/kev-img.png"
+quality_x = 300 # The quality of the image, based on how many columns there are
 
 if __name__ == "__main__":
     img = render(image_path)
-    print(img)
+    #print(img)
     new_img = generate_new_image(img, quality_x)
-    plt.imshow(new_img)
-    plt.show()
+    #plt.imshow(new_img)
+    #plt.show()
