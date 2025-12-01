216f0445de3029e050c5f6fc60e4d9f23539fb87
JoeH2025 <joseph.robert.huber@gmail.com>
2025-12-01 14:12:26 -0600

    comments n whitespace

--- develop_modified_image.py
+++ develop_modified_image.py
@@ -2,13 +2,18 @@ from rsa.common import ceil_div
 
 
 def generate_new_image(og_img, new_x):
+    #Generate the necessary components
+    #Original dimensions
     og_dimensions = get_dimensions(og_img)
     og_x = og_dimensions[0]
     og_y = og_dimensions[1]
 
     new_y = int(og_y * new_x / og_x)
 
+    #subspace dimensions for averaged pixels
     subspace_x = ceil_div(og_x, new_x)
     subspace_y = ceil_div(og_y, new_y)
+
+
 def get_dimensions(arr):
     return [len(arr), len(arr[0])]
