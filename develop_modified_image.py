c150f35b645c10a3e39840297903b66130af0acc
JoeH2025 <joseph.robert.huber@gmail.com>
2025-12-01 14:10:44 -0600

    apply rect logic

--- develop_modified_image.py
+++ develop_modified_image.py
@@ -1,10 +1,13 @@
 from rsa.common import ceil_div
 
 
-def generate_new_image(og_img, new_x, new_y):
+def generate_new_image(og_img, new_x):
     og_dimensions = get_dimensions(og_img)
     og_x = og_dimensions[0]
     og_y = og_dimensions[1]
+
+    new_y = int(og_y * new_x / og_x)
+
     subspace_x = ceil_div(og_x, new_x)
     subspace_y = ceil_div(og_y, new_y)
 def get_dimensions(arr):
