ad21933854e48ee071702ea65aec35957bb98531
JoeH2025 <joseph.robert.huber@gmail.com>
2025-12-05 17:10:22 -0600

    add main image

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from develop_modified_image import generate_new_image
 from color_print import print_colored_image
 import matplotlib.pyplot as plt
 
-image_path = "images/kev-img.png"
-quality_x = 300 # The quality of the image, based on how many columns there are
+image_path = "images/renderer-image.png"
+quality_x = 150 # The quality of the image, based on how many columns there are
 
 if __name__ == "__main__":
     img = render(image_path)
